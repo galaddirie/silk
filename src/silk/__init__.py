aa049c0d296e05bf151d1da79e30d0a500840ab0
Galad Dirie <galad.work@gmail.com>
2025-05-27 13:17:11 -0400

    chore: prepare release v0.3.0

--- src/silk/__init__.py
+++ src/silk/__init__.py
@@ -2,7 +2,7 @@
 Silk - A flexible browser automation library
 """
 
-__version__ = "0.2.5"
+__version__ = "0.3.0"
 
 from expression import Error, Nothing, Ok, Option, Result, Some  # noqa
 from fp_ops import operation  # noqa
